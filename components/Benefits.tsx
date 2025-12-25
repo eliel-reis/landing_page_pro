1cd0c0ea1bad2803451734db07538a13adc3193d
Eliel Reis <elielreis651@gmail.com>
2025-12-25 04:55:33 -0300

    adicionei sm e ml para melhor responsividade.

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -2,7 +2,7 @@ import { siteConfig } from "@/config/site";
 
 export default function Benefits() {
   return (
-    <section className="py-16 max-w-3xl mx-auto">
+    <section className="py-16 max-w-3xl sm:mx-auto ml-8">
       <ul className="space-y-3">
         {siteConfig.benefits.map((item, index) => (
           <li key={index} className="text-lg">
